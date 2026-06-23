a921f5acfe3db47d6bb642e7144dbff2b021475c
hattila08 <helyes.attila08@gmail.com>
2026-06-23 13:37:14 +0200

    feat: integrate SQLite database with SQLAlchemy

---
+++ database.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "sqlite:///./books.db"
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+class DBBokk(Base):
+    __tablename__ = "books"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    author = Column(String, index=True)
+    year = Column(Integer, index=True)
+
+Base.metadata.create_all(bind=engine)
--- main.py
+++ main.py
@@ -1,26 +1,41 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
+from sqlalchemy.orm import Session
+from database import SessionLocal, DBBook
 
 app = FastAPI(title = "Könyvtár API")
 
-class Book(BaseModel):
-    id: int
+class BookCreate(BaseModel):
     title: str
     author: str
     year: int
 
+class BookResponse(BaseModel):
+    id: int
+
+    class Config:
+        from_attributes = True
+
 # ideiglenes "adatbázis"
-fake_db = []
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
 def read_root():
     return {"message": "Üdvözlünk a Könyvtár API-ban!"}
 
-@app.post("/books/")
-def create_book(book: Book):
-    fake_db.append(book)
-    return {"message": "Könyv hozzáadva", "book": book}
+@app.post("/books/", response_model=BookResponse)
+def create_book(book: BookCreate, db: Session = next(get_db())):
+    db_book = DBBook(title=book.title, author=book.author, year=book.year)
+    db.add(db_book)
+    db.commit()
+    db.refresh(db_book)
+    return db_book
 
-@app.get("/books/")
-def get_books():
-    return fake_db
+@app.get("/books/", response_model=list[BookResponse])
+def get_books(db: Session = Depends(get_db)):
+    return db.query(DBBook).all()
