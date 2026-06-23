eb156a4190018ecd94de74c4f4d01c4c247db226
hattila08 <helyes.attila08@gmail.com>
2026-06-23 17:26:10 +0200

    feat: added get book by id function

--- main.py
+++ main.py
@@ -39,3 +39,10 @@ def create_book(book: BookCreate, db: Session = Depends(get_db)):
 @app.get("/books/", response_model=list[BookResponse])
 def get_books(db: Session = Depends(get_db)):
     return db.query(DBBook).all()
+
+@app.get("/books/{book_id}", response_model=BookResponse)
+def get_book(book_id: int, db: Session = Depends(get_db)):
+    db_book = db.query(DBBook).where(DBBook.id == book_id).first()
+    if db_book is None:
+        raise HTTPException(status_code=404, detail="Könyv nem található")
+    return db_book
